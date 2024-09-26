38890c6890999ca9490ffcbe2666b43e45b37541
soohyun <sss4920@naver.com>
2024-09-27 01:52:51 +0900

    Refactor: Treemap을 통한 map정렬 문제 클래스 네임 수정

--- backjoon/BJ7785.java
+++ backjoon/BJ7785.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.TreeMap;
 
-public class Main {
+public class BJ7785 {
 	static TreeMap<String, String> map = new TreeMap<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
