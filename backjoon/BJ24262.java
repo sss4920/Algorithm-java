5c4f2e476fcfc75e3002f719f91a967db3770f40
soohyun <sss4920@naver.com>
2024-10-04 17:11:48 +0900

    Feat: 알고리즘 시간으로 낚시문제 백준

---
+++ backjoon/BJ24262.java
@@ -0,0 +1,10 @@
+package backjoon;
+
+public class BJ24262 {
+	public static void main(String[] args) {
+		int count =1;
+		int degree = 0;
+		System.out.println(count);
+		System.out.println(degree);
+	}
+}
