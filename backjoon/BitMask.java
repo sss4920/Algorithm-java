b931fcaa15a0aff93dcedf507f09d6f362f947ef
soohyun <sss4920@naver.com>
2025-01-30 15:06:30 +0900

    Feat: 비트마스크 이론 주석 추가

--- backjoon/BitMask.java
+++ backjoon/BitMask.java
@@ -3,7 +3,10 @@ package backjoon;
 public class BitMask {
 	public static void main(String[] args) {
 		int A = 0;
-		A = (1<<10);
+		A = (1<<10); //1을 왼쪽으로 10칸을 밀고, 민 곳에 0으로 채운다.
+		//이는 1 * 2**10과 같다.
 		System.out.println(Integer.toBinaryString(A));
+
+
 	}
 }
