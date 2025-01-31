0971d17d9285c4c6079f68e9e40b8e64f939fef0
soohyun <sss4920@naver.com>
2025-01-31 15:41:24 +0900

    Feat: 비트마스크로 집합 구현풀기 다시공부

--- backjoon/BJ11723.java
+++ backjoon/BJ11723.java
@@ -3,81 +3,43 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.List;
+
 
 public class BJ11723 {
-	public static HashSet<Integer> set = new HashSet<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		int x = 0;
 		for (int i=0;i<a;i++){
 			String[] operation = br.readLine().split(" ");
 			switch (operation[0]){
 				case "add":
-					int x = Integer.parseInt(operation[1]);
-					add(x);
+					int k = Integer.parseInt(operation[1]);
+					x |= (1<< k);
 					break;
 				case "check":
-					x = Integer.parseInt(operation[1]);
-					sb.append(check(x));
-					sb.append("\n");
+					k = Integer.parseInt(operation[1]);
+					sb.append((x & (1<<k)) != 0?1:0).append("\n");
 					break;
 				case "remove":
-					x = Integer.parseInt(operation[1]);
-					remove(x);
+					k = Integer.parseInt(operation[1]);
+					x &= ~(1<<k);
 					break;
 				case "toggle":
-					x = Integer.parseInt(operation[1]);
-					toggle(x);
+					k = Integer.parseInt(operation[1]);
+					x ^= (1<<k); //얘가 이해가 잘안감.
 					break;
 				case "all":
-					all();
+					x = (1 << 21) -1; // 21칸 까지 밀면 100000.. -> 1빼면 22번째줄 사라지면서 01111111..되겠지.
 					break;
-				case "clear":
-					clear();
+				case "empty":
+					x = 0;
 					break;
 				default:
 					break;
 			}
 		}
 		System.out.println(sb);
-	}
-
-	public static void add(int x){
-		if (check(x)==0){
-			set.add(x);
-		}
-	}
-
-	public static void remove(int x){
-		if (check(x)==1){
-			set.remove(x);
-		}
-	}
-
-	public static int check(int x){
-		if (set.contains(x)){
-			return 1;
-		}
-		return 0;
-	}
-
-	public static void toggle(int x){
-		if (check(x)==1){
-			remove(x);
-		}else{
-			add(x);
-		}
-	}
-
-	public static void all(){
-		set = new HashSet<>();
-		set.addAll(List.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
-	}
-
-	public static void clear(){
-		set.clear();
 	}
 }
